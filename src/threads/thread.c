5c1923a1a3cf931d955dc334aae191f60bf59521
耿琮凯 <gengcongkai789123@163.com>
2019-11-23 20:35:21 +0800

    test

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -485,7 +485,7 @@ init_thread (struct thread *t, const char *name, int priority)
   strlcpy (t->name, name, sizeof t->name);
   t->stack = (uint8_t *) t + PGSIZE;
   t->priority = priority;
-  t->locks = NULL;
+  list_init(t->locks);
   t->base_priority = priority;
   t->magic = THREAD_MAGIC;
 
