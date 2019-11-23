77d4acf72007c5066ed6b683c3af150b4c4aa78c
耿琮凯 <gengcongkai789123@163.com>
2019-11-23 20:53:06 +0800

    test

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -116,7 +116,7 @@ sema_up (struct semaphore *sema)
   if (!list_empty (&sema->waiters)) {
     thread_unblock (list_entry (list_pop_front (&sema->waiters),
                                 struct thread, elem));
-    thread_yield();
+    //thread_yield();
   }
   sema->value++;
   intr_set_level (old_level);
