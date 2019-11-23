97c071860910e4916f313fae1b03dcd4b1c340ce
耿琮凯 <gengcongkai789123@163.com>
2019-11-23 20:18:02 +0800

    test

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -195,7 +195,7 @@ lock_acquire (struct lock *lock)
   ASSERT (lock != NULL);
   ASSERT (!intr_context ());
   ASSERT (!lock_held_by_current_thread (lock));
-  lock->holder->priority = (lock->holder->priority > thread_current ()->priority ? lock->holder->priority : thread_current() ->priority);
+  //lock->holder->priority = (lock->holder->priority > thread_current ()->priority ? lock->holder->priority : thread_current() ->priority);
 
   sema_down (&lock->semaphore);
   lock->holder = thread_current ();
@@ -232,7 +232,7 @@ lock_release (struct lock *lock)
   ASSERT (lock != NULL);
   ASSERT (lock_held_by_current_thread (lock));
   
-  lock->holder->priority = lock->holder->base_priority;
+//  lock->holder->priority = lock->holder->base_priority;
   lock->holder = NULL;
   sema_up (&lock->semaphore);
 }
